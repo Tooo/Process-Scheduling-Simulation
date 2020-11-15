289cc394cb12c2eedcce93b2581cb3cf6ae082af
tsc7 <tsc7@sfu.ca>
2020-11-14 22:05:41 -0800

    Process h file

---
+++ process.c
@@ -0,0 +1,6 @@
+#include "process.h"
+#include "list.h"
+
+static List * highQueue;
+static List * normQueue;
+static List * lowQueue;
---
+++ process.h
@@ -0,0 +1,31 @@
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+
+typedef struct PCB_s PCB;
+struct PCB_s {
+    int PID;
+    int priority;
+    int state;
+    char * message;
+};
+
+enum STATE {
+    PROCESS_RUNNING,
+    PROCESS_READY,
+    PROCESS_BLOCKED
+};
+
+void Process_createQueues();
+
+void Process_create(int priority);
+
+void Process_fork();
+
+void Process_kill(int pid);
+
+void Process_exit();
+
+void Process_quantum();
+
+
+#endif
