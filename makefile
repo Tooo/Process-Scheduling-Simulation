ba069adfab778aaa4e93602c52ea1dfd5d15e3c5
tsc7 <tsc7@sfu.ca>
2020-11-14 21:41:00 -0800

    Create c and h files for other commands

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror
 
-all: main.o list.o textmenu.o
-	gcc $(CFLAGS) main.o list.o textmenu.o
+all: main.o list.o textmenu.o process.o message.o semaphore.o
+	gcc $(CFLAGS) main.o list.o textmenu.o process.o message.o semaphore.o
 
 main.o: main.c
 	gcc $(CFLAGS) -c main.c
@@ -11,6 +11,15 @@ list.o: list.c
 
 textmenu.o: textmenu.c
 	gcc $(CFLAGS) -c textmenu.c
+
+process.o: process.c
+	gcc $(CFLAGS) -c process.c
+
+message.o: message.c
+	gcc $(CFLAGS) -c message.c
+
+semaphore.o: semaphore.c
+	gcc $(CFLAGS) -c semaphore.c
 
 clean:
 	rm a.out *.o
--- textmenu.c
+++ textmenu.c
@@ -14,10 +14,10 @@ void printIntroduction() {
     printf("** PCB's and Processing Scheduling Simulation **\n");
     printf("* Process Commands *\n");
     printf("C - Create, F - Fork, K - Kill, E - Exit, Q - Quantum\n");
-    printf("* Messaging Commands *\n");
+    printf("* Message Commands *\n");
     printf("S - Send, R - Receive, Y - Reply\n");
     printf("* Semaphore Commands *\n");
-    printf("N - New Sempahore, P - Semapahore P, V - Sempahore V\n");
+    printf("N - New Semaphore, P - Semaphore P, V - Semaphore V\n");
     printf("* Info Commands *\n");
     printf("I - Procinfo, T - Totalinfo\n");
 }
