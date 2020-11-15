c24d485c5bc0fa7b6519b63494226a4b953f8f72
tsc7 <tsc7@sfu.ca>
2020-11-14 20:34:54 -0800

    Create switch case

--- main.c
+++ main.c
@@ -1,4 +1,34 @@
 
 int main () {
-    
+    char input;
+    switch (input) {
+        case 'C':
+            break;  
+        case 'F':
+            break;
+        case 'K':
+            break;
+        case 'E':
+            break;
+        case 'Q':
+            break;
+        case 'S':
+            break;
+        case 'R':
+            break;
+        case 'Y':
+            break;
+        case 'N':
+            break;
+        case 'P':
+            break;
+        case 'V':
+            break;
+        case 'I':
+            break;
+        case 'T':
+            break;
+        default:
+        }
+    }
 }
