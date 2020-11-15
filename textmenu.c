528ad002b035ce3f42b0746cf3d60370c7c03602
tsc7 <tsc7@sfu.ca>
2020-11-14 20:45:29 -0800

    printIntroduction

---
+++ textmenu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printIntroduction() {
+    printf("** PCB's and Processing Scheduling Simulation **");
+    printf("* Process Commands *");
+    printf("C - Create, F - Fork, K - Kill, E - Exit, Q - Quantum");
+    printf("* Messaging Commands *");
+    printf("S - Send, R - Receive, Y - Reply");
+    printf("* Semaphore Commands *");
+    printf("N - New Sempahore, P - Semapahore P, V - Sempahore V");
+    printf("* Info Commands *");
+    printf("I - Procinfo, T - Totalinfo");
+}
